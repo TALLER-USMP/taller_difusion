344c428c5aa0a837fe195de2ef1466af477d7c30
Leonardo-YC <leonardoyupan2012@gmail.com>
2025-11-03 06:02:58 -0500

    ULTIMA

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 // src/App.jsx
-import { Outlet } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+import Home from './pages/Home'
+import Proyecto from './pages/Proyecto'
+import Equipo from './pages/Equipo'
+import Avances from './pages/Avances'
+import Arquitectura from './pages/Arquitectura'
+import Wiki from './pages/Wiki'
 import './styles/globals.css'
 
 function App() {
@@ -10,9 +16,16 @@ function App() {
       <Navbar />
       
       <main className="flex-grow">
-        <Outlet />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/proyecto" element={<Proyecto />} />
+          <Route path="/equipo" element={<Equipo />} />
+          <Route path="/avances" element={<Avances />}/>
+          <Route path="/arquitectura" element={<Arquitectura />}/>
+          <Route path="/wiki" element={<Wiki />} />
+        </Routes>
       </main>
-
+      
       <Footer />
     </div>
   )
