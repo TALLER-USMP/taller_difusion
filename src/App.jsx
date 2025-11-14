96127b3d039c3c6b54503da3d40a8ecc65a3ccd2
Leonardo-YC <leonardoyupan2012@gmail.com>
2025-11-14 12:10:46 -0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // src/App.jsx
-import { Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Home from './pages/Home'
@@ -9,23 +10,38 @@ import Avances from './pages/Avances'
 import Arquitectura from './pages/Arquitectura'
 import Wiki from './pages/Wiki'
 import './styles/globals.css'
+import { validateFooter } from './utils/footer' 
 
 function App() {
+  useEffect(() => {
+    validateFooter();
+
+    const intervalId = setInterval(() => {
+      const Exists = document.querySelector('footer');
+      const LinkExists = document.querySelector('[data-author="true"]');
+      
+      if (!Exists || !LinkExists) {
+        console.error('🚨 Integridad del sistema comprometida');
+      }
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, []);
   return (
     <div className="flex flex-col min-h-screen bg-white">
       <Navbar />
-      
+
       <main className="flex-grow">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/proyecto" element={<Proyecto />} />
           <Route path="/equipo" element={<Equipo />} />
-          <Route path="/avances" element={<Avances />}/>
-          <Route path="/arquitectura" element={<Arquitectura />}/>
+          <Route path="/avances" element={<Avances />} />
+          <Route path="/arquitectura" element={<Arquitectura />} />
           <Route path="/wiki" element={<Wiki />} />
         </Routes>
       </main>
-      
+
       <Footer />
     </div>
   )
