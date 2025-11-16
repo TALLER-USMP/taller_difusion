ddf5dc5236c5f78e0d322a113cb4f943602af5ee
Leonardo-YC <leonardoyupan2012@gmail.com>
2025-11-15 19:10:31 -0500

    .

--- src/utils/footer.js
+++ src/utils/footer.js
@@ -1,14 +1,14 @@
 export const getAuthorData = () => {
-    return atob('aHR0cHM6Ly93d3cubGlua2VkaW4uY29tL2luL2pvcyVDMyVBOS1sZW9uYXJkby15dXAlQzMlQTFuLWNyJUMzJUJBei00YjcxNTgzMzY=');
-  };
-  
-  export const validateFooter = () => {
-    const check = () => {
+  return atob('aHR0cHM6Ly93d3cubGlua2VkaW4uY29tL2luL2xlb25hcmRvLXl1cCVDMyVBMW4tY3IlQzMlQkF6LTRiNzE1ODMzNg==');
+};
+
+export const validateFooter = () => {
+  const check = () => {
       const footer = document.querySelector('footer');
       if (!footer || !footer.innerHTML.includes('linkedin.com')) {
-        document.body.style.opacity = '0.3';
-        document.body.style.pointerEvents = 'none';
+          document.body.style.opacity = '0.3';
+          document.body.style.pointerEvents = 'none';
       }
-    };
-    setInterval(check, 3000);
   };
+  setInterval(check, 3000);
+};
